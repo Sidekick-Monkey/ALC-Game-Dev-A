b2f4b6f26bb0167488c8280b54eb90318ed64488
Alejandro <alejandro.d373@stu.nebo.edu>
2020-10-19 12:38:45 -0600

    Prototype 2 animal stampede

--- Game Dev/Prototype 2 Animal Stampede/Assets/Scripts/PlayerController.cs
+++ Game Dev/Prototype 2 Animal Stampede/Assets/Scripts/PlayerController.cs
@@ -6,26 +6,38 @@ public class PlayerController : MonoBehaviour
 {
 public float horizontalInput;
 public float speed = 10.0f;
+public float xRange = 15.0f;
+
+public GameObject foodProjectile;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+    
      horizontalInput = Input.GetAxis("Horizontal");
      transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+     
    // Constrains  Players Movement to the left at -10 
-   if(transform.position.x < -10)
+     if(transform.position.x < -xRange)
     {
-    transform.position = new Vector3(-10, transform.position.y,transform.position.z);
-    
+       transform.position = new Vector3(-xRange, transform.position.y,transform.position.z);
     }
-     // Constrains  Players Movement to the left at -10 
-   if(transform.position.x < -10)
-    {
-    transform.position = new Vector3(-10, transform.position.y,transform.position.z);
+    
+     if(transform.position.x < xRange)
+     {
+        transform.position = new Vector3(xRange,transform.position.y,transform.position.z);
+     }
+     if(Input.GetKeyDown(KeyCode.Space))
+     {
+        Instantiate(foodProjectile, transform.position, foodProjectile.transform.rotation);
+     }
+        
+        
+     
     }
 }
---
+++ Game Dev/Prototype 2 Animal Stampede/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20f;
+    private float spawwnPosZ = 30f;
+    
+    private float startDelay = 2.0f;
+    private float SpawnInterval = 1.5f;
+    void Start()
+    {
+        InvokeRepeating("SpawnRandAnimal", startDelay, spawnIntervenal)
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnPosZ);
+        
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Inistiate(animalPrefabs{animalIndex},spawnPos, animalPrefabs{animalIndex}.transform.rotation)
+    
+    }
+}
